bfa39831a71967e832f272ee0dac5e70f17c6fc6
Naufal Irawan <irawanaufal29209@gmail.com>
2024-05-07 00:34:15 +0700

    Second commit , forgot add DB

---
+++ sismul-waifuku.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 06, 2024 at 07:33 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sismul-waifuku`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `waifuku`
+--
+
+CREATE TABLE `waifuku` (
+  `kode` int(9) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `asal` varchar(30) NOT NULL,
+  `gambar` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `waifuku`
+--
+
+INSERT INTO `waifuku` (`kode`, `nama`, `asal`, `gambar`) VALUES
+(1, 'Elinalise', 'Mushoku Tensei', 'Elinalise.png'),
+(2, 'Kato Megumi', 'Saekano', 'Kato_Megumi.jpeg'),
+(3, 'Rias Gremory', 'Highschool DxD', 'Rias_Gremory.png'),
+(4, 'Shinomiya Kaguya', 'Love is war', 'Shinomiya_Kaguya.jpeg'),
+(5, 'Yor Forger', 'Spy x Family', 'Yor_Forger.png');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `waifuku`
+--
+ALTER TABLE `waifuku`
+  ADD PRIMARY KEY (`kode`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `waifuku`
+--
+ALTER TABLE `waifuku`
+  MODIFY `kode` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
